9d6acae1306f7db41309eb0bda9552471fd10c14
Federico Campero <89789363+frcampero@users.noreply.github.com>
2025-06-18 00:05:56 -0300

    Added functions to user experience

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path' 
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['path'],
     },
   },
 })
